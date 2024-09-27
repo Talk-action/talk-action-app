41a38d576de3556ec13cb48ef9ed2d851891f363
Gabriel dos Santos <gabrieldsc2356@gmail.com>
2024-09-27 20:37:09 -0300

    Update Cadastro.jsx

--- src/Pages/cadastro/Cadastro.jsx
+++ src/Pages/cadastro/Cadastro.jsx
@@ -1,12 +1,52 @@
-function Cadastro () {
+import style from '../cadastro/cadastro.module.css'
+import logo from '../../assets/logo.png'
 
 
+function cadastro () {
     return(
-        <div>
-            <h2>Cadastro</h2>
+        <div className={style.container}>
+            <section className={style.cadastro}>
+            
+                <div className={style.cadastroName}>
+
+                    <p className={style.p}>Cadastre-se</p>
+                </div>   
+                <div className={style.containercadastro}> 
+                    <div>
+                        <div className={style.containercadastro}>
+                        <form className={style.buttonForms}>
+                            <input className={style.input} type='email' placeholder='Email' required/>
+                            <input className={style.input} type='password' placeholder='Senha' required/>
+                            <input className={style.input} type='password' placeholder='Repita a senha' required />
+                        
+                            <button className={style.button}>Criar</button>
+                        </form>
+
+                            <div className={style.containerRegistro}>
+                                <div className={style.border}></div>
+
+                                
+
+                                <div className={style.border}></div>
+
+                            
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                    
+            </div>
+
+
+            </section>
+
+            <section className={style.logo}>
+                <img src={logo} className={style.imagem}/>
+            </section>
         </div>
     )
-    
 }
 
-export default Cadastro
+
+export default cadastro
